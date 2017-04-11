f15be3240d4929697f0ffa910f3f0c11df4d93cf
John Flynn <john@catchfirecreative.com>
2017-04-11 15:21:35 -0400

    Add Cloudflare cache clear Quicksilver hook

---
+++ pantheon/clear-cloudflare-quicksilver/README.md
@@ -0,0 +1,31 @@
+# Cloudflare Cache #
+
+This Quicksilver script will purge the Cloudflare cache when your live environment's cache is cleared from the Pantheon dashboard or the WP Admin
+
+## Instructions ##
+
+- Copy `cloudflare_cache.json` to `files/private` of the *live* environment and update it with the cloudflare info.
+ - API key can be found in the `My Settings` page on Cloudflare. (DO NOT reset the API key)
+ - Zone ID can be found the `Overview` page of the Cloudflare site dashboard
+- Add the `cloudflare_cache.php` script to the `private/scripts` directory of your code repository.
+- Add a Quicksilver operation to your `pantheon.yml` to fire the script after a deploy.
+  - More info on `pantheon.yml` found [here](https://pantheon.io/docs/pantheon-yml/)
+- Deploy through to the live environment and clear the cache!
+
+Optionally, you may want to use the `terminus workflows watch` command to get immediate debugging feedback.
+
+### Example `pantheon.yml` ###
+
+Here's an example of what your `pantheon.yml` would look like if this were the only Quicksilver operation you wanted to use:
+
+```yaml
+api_version: 1
+php_version: 7.0
+
+workflows:
+  clear_cache:
+    after:
+      - type: webphp
+        description: Cloudflare Cache
+        script: private/scripts/cloudflare_cache.php
+```
---
+++ pantheon/clear-cloudflare-quicksilver/cloudflare_cache.php
@@ -0,0 +1,40 @@
+<?php
+// TODO: this header should not be required...
+header('Content-Type: text/plain; charset=UTF-8');
+// Only purge Cloudflare cache when the live environment's cache is cleared.
+if ($_ENV['PANTHEON_ENVIRONMENT'] != 'live') {
+  die();
+}
+// Retrieve Cloudflare config data
+$config_file = $_SERVER['HOME'] . '/files/private/cloudflare_cache.json';
+$config = json_decode(file_get_contents($_SERVER['HOME'] . '/files/private/cloudflare_cache.json'), 1);
+if ($config == FALSE) {
+  die('files/private/cloudflare_cache.json not found. Aborting!');
+}
+
+purge_cache($config);
+
+/**
+ * Send a request to CloudFlare's API to purge its cache for the site
+ * 
+ * @param  array $config The Cloudflare credentials
+ */
+function purge_cache($config) {
+  // Set the CloudFlare API action
+  $payload = json_encode(array('purge_everything' => TRUE));
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, 'https://api.cloudflare.com/client/v4/zones/' . $config['zone_id'] . '/purge_cache');
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'X-Auth-Email: ' . $config['email'],
+    'X-Auth-Key: ' . $config['api_key'],
+    'Content-Type: application/json'
+  ));
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+  // Go, Go Gadget Purge!
+  print("\n==== Sending Request to Cloudflare ====\n");
+  $result = curl_exec($ch);
+  print("RESULT: $result");
+  print("\n===== Request Complete! =====\n");
+  curl_close($ch);
+}
