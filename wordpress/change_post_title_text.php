f83062069303a4560653f446692c23eb78177323
John Flynn <john@catchfirecreative.com>
2017-07-10 09:23:59 -0400

    add change title text

---
+++ wordpress/change_post_title_text.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Changes the default 'Enter Title Here'
+ * text on the post edit screen to a
+ * phrase of your choosing
+ */
+
+ function post_type_change_title_text( $title ){
+     $screen = get_current_screen();
+      
+     // Edit here!
+     if  ( 'post_type' == $screen->post_type ) {
+          $title = 'Product Title';
+     }
+ 
+     return $title;
+}
+ 
+add_filter( 'enter_title_here', 'post_type_change_title_text' );
