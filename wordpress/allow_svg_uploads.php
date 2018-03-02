7df31d0532b72673a7a173e7f98597cda9496f4d
John Flynn <JJimmyFlynn@gmail.com>
2018-03-02 11:11:21 -0500

    Update allow_svg_uploads.php

--- wordpress/allow_svg_uploads.php
+++ wordpress/allow_svg_uploads.php
@@ -4,9 +4,6 @@
  * the WordPress media uploader
  */
 
- /**
- * Allow SVG through WP media uploader
- */
 function add_svg_to_upload_mimes( $upload_mimes ) {
   $upload_mimes['svg'] = 'image/svg+xml';
   $upload_mimes['svgz'] = 'image/svg+xml';
