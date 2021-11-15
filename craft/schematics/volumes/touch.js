8483173e09bfe5db1f67dd9fb36cdd79771fddca
John Flynn <JJimmyFlynn@gmail.com>
2021-11-14 22:23:16 -0500

    add schematics for craft

---
+++ craft/schematics/volumes/touch.js
@@ -0,0 +1,27 @@
+{
+    "volumes": [
+        {
+            "name": "Images",
+            "handle": "images",
+            "type": "craft\\volumes\\Local",
+            "hasUrls": true,
+            "url": "@web\/media\/images",
+            "settings": {
+                "path": "media\/images"
+            },
+            "fieldLayout": {
+                "Content": [
+                    "__ui_1"
+                ]
+            },
+            "fieldConfigs": {
+                "Content": {
+                    "__ui_1": {
+                        "type": "craft\\fieldlayoutelements\\AssetTitleField",
+                        "width": 100
+                    }
+                }
+            }
+        }
+    ]
+}
